3831f88e51309735a082fb425eda3a06d9427c08
josiahu <jyoo@business-software.com>
2017-07-06 14:44:31 -0500

    added active page

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -7,13 +7,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a id=basic class="navbar-brand" href="#">Basic</a>
+      <a id=basic class="navbar-brand">Basic</a>
     </div>
     <div id="navbar" class="collapse navbar-collapse">
       <ul id="titles" class="nav navbar-nav">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li class="{{Request::is('/') ? 'active' : ''}}"><a href="/">Home</a></li>
+        <li class="{{Request::is('about') ? 'active' : ''}}"><a href="/about">About</a></li>
+        <li class="{{Request::is('contact') ? 'active' : ''}}"><a href="/contact">Contact</a></li>
       </ul>
     </div><!--/.nav-collapse -->
   </div>
